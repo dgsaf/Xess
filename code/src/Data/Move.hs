154403be5e737f4cd9a66cc3df4a0e4c7a62cb0c
Tom Ross <tom.ross@protonmail.com>
2022-07-11 11:49:10 +0800

    [UPD] Implement different move encoding

--- code/src/Data/Move.hs
+++ code/src/Data/Move.hs
@@ -3,7 +3,14 @@ Module      : Data.Move
 Description :
 -}
 module Data.Move
-  (
+  ( Move
+  , castle
+  , quiet
+  , push
+  , promote
+  , capture
+  , captureEP
+  , capturePromote
   ) where
 
 import Data.Castle
@@ -19,136 +26,116 @@ import Data.Ix
 import Data.Maybe
 import Data.Word
 
--- | Encoded Capture Types
-data CaptureT
-  = CaptureP
-  | CaptureN
-  | CaptureB
-  | CaptureR
-  | CaptureQ
-  | CaptureEP
-  deriving (Eq, Ord, Ix, Enum, Bounded, Read, Show)
-
-encodeCaptureT :: Piece -> CaptureT
-encodeCaptureT P = CaptureP
-encodeCaptureT N = CaptureN
-encodeCaptureT B = CaptureB
-encodeCaptureT R = CaptureR
-encodeCaptureT Q = CaptureQ
-
-decodeCaptureT :: CaptureT -> Piece
-decodeCaptureT CaptureP  = P
-decodeCaptureT CaptureN  = N
-decodeCaptureT CaptureB  = B
-decodeCaptureT CaptureR  = R
-decodeCaptureT CaptureQ  = Q
-decodeCaptureT CaptureEP = P
-
-squaresEP :: Square -> Maybe (Square, Square)
-squaresEP sqEP
-  | y == 2    = Just (toEnum $ n - 8, toEnum $ n + 8)
-  | y == 5    = Just (toEnum $ n + 8, toEnum $ n - 8)
-  | otherwise = Nothing
-  where
-    y = coordY sqEP
-    n = fromEnum sqEP
-
-originCapture :: Square -> CaptureT -> Square
-originCapture sq CaptureEP = snd . fromJust . squaresEP $ sq
-originCapture sq _         = sq
-
--- | Encoded Promotion Types
-data PromoteT
-  = PromoteN
+-- |
+data MoveFlag
+  = CastleWS
+  | CastleWL
+  | CastleBS
+  | CastleBL
+  | Quiet
+  | Push
+  | PromoteN
   | PromoteB
   | PromoteR
   | PromoteQ
-  deriving (Eq, Ord, Ix, Enum, Bounded, Read, Show)
+  | Capture
+  | CaptureEP
+  | CapturePromoteN
+  | CapturePromoteB
+  | CapturePromoteR
+  | CapturePromoteQ
 
-decodePromoteT :: PromoteT -> Piece
-decodePromoteT PromoteN = N
-decodePromoteT PromoteB = B
-decodePromoteT PromoteR = R
-decodePromoteT PromoteQ = Q
-
-promoteMask :: Word64
-promoteMask = (lineY ! (toEnum 0)) .|. (lineY ! (toEnum 56))
-
--- | Move
 data Move
-  = Normal (Square, Piece) (Square, Maybe CaptureT) (Maybe PromoteT)
-  | Castle (Colour, Side)
-  deriving (Eq, Read, Show)
-
-origin :: Move -> Square
-origin (Normal (sq, p) (sq', mct) (mpt)) = sq
-origin (Castle (c, s)) = fst . castleSquaresK $ (c, s)
-
-target :: Move -> Square
-target (Normal (sq, p) (sq', mct) (mpt)) = sq'
-target (Castle (c, s)) = snd . castleSquaresK $ (c, s)
-
-moving :: Move -> Piece
-moving (Normal (sq, p) (sq', mct) (mpt)) = p
-moving (Castle (c, s)) = K
-
-capturing :: Move -> Maybe Piece
-capturing (Normal (sq, p) (sq', mct) (mpt)) = fmap decodeCaptureT mct
-capturing (Castle (c, s)) = Nothing
-
-promotingTo :: Move -> Maybe Piece
-promotingTo (Normal (sq, p) (sq', mct) (mpt)) = fmap decodePromoteT mpt
-promotingTo (Castle (c, s)) = Nothing
-
--- | Constructor
-quiet :: (Square, Piece) -> Square -> Move
-quiet (sq, p) sq' = Normal (sq, p) (sq', Nothing) Nothing
-
-quietPromote :: (Square, Piece) -> Square -> PromoteT -> Move
-quietPromote (sq, p) sq' pt = Normal (sq, p) (sq', Nothing) (Just pt)
-
-capture :: (Square, Piece) -> (Square, CaptureT) -> Move
-capture (sq, p) (sq', ct) = Normal (sq, p) (sq', Just ct) Nothing
-
-capturePromote :: (Square, Piece) -> (Square, CaptureT) -> PromoteT -> Move
-capturePromote (sq, p) (sq', ct) pt = Normal (sq, p) (sq', Just ct) (Just pt)
+  = Move
+  { _origin :: Square
+  , _target :: Square
+  , _moveFlag :: MoveFlag
+  , _captured :: Maybe (Colour, Piece)
+  }
 
 castle :: (Colour, Side) -> Move
-castle (c, s) = Castle (c, s)
+castle (c, s) =
+  Move
+  { _origin = sqK
+  , _target = sqK'
+  , _moveFlag = flag
+  , _captured = Nothing
+  }
+  where
+    (sqK, sqK') = castleSquaresK (c, s)
+    flag =
+      case (c, s) of
+        (White, Short) -> CastleWS
+        (White, Long)  -> CastleWL
+        (Black, Short) -> CastleBS
+        (Black, Long)  -> CastleBL
 
--- |
-quietMoves :: Board -> Square -> (Colour, Piece) -> Word64 -> [Move]
-quietMoves b sq (c, P) moveMask = qs ++ concat (fmap qps [minBound .. maxBound])
-  where
-    maskQ  = moveMask .&. unoccupied b .&. (complement promoteMask)
-    maskQP = moveMask .&. unoccupied b .&. promoteMask
-    qs     = fmap (quiet (sq, P)) $ decodeSquares maskQ
-    qps pt = fmap (\ sq' -> quietPromote (sq, P) sq' pt) $ decodeSquares maskQP
-quietMoves b sq (c, p) moveMask = qs
-  where
-    maskQ = moveMask .&. unoccupied b
-    qs = fmap (quiet (sq, p)) $ decodeSquares maskQ
+quiet :: Square -> Square -> Move
+quiet sq sq' =
+  Move
+  { _origin = sq
+  , _target = sq'
+  , _moveFlag = Quiet
+  , _captured = Nothing
+  }
 
-captureMoves :: Board -> Square -> (Colour, Piece) -> Word64 -> [Move]
-captureMoves b sq (c, P) moveMask = cs ++ concat (fmap cps [minBound .. maxBound])
-  where
-    maskC  = moveMask .&. hostile b c .&. (complement promoteMask)
-    maskCP = moveMask .&. hostile b c .&. promoteMask
-    ct sq' = encodeCaptureT . snd . fromJust $ b !? sq'
-    cs     = fmap (\ sq' -> capture (sq, P) (sq', ct sq'))
-             $ decodeSquares maskC
-    cps pt = fmap (\ sq' -> capturePromote (sq, P) (sq', ct sq') pt)
-             $ decodeSquares maskC
-captureMoves b sq (c, p) moveMask = cs
-  where
-    maskC = moveMask .&. hostile b c
-    ct sq' = encodeCaptureT . snd . fromJust $ b !? sq'
-    cs = fmap (\ sq' -> capture (sq, p) (sq', ct sq')) $ decodeSquares maskC
+push :: Square -> Square -> Move
+push sq sq' =
+  Move
+  { _origin = sq
+  , _target = sq'
+  , _moveFlag = Push
+  , _captured = Nothing
+  }
 
-enPassantMoves :: Board -> Square -> (Colour, Piece) -> Word64 -> Square -> [Move]
-enPassantMoves b sq (c, P) moveMask sqEP
-  | maskEP /= zeroBits = [capture (sq, P) (sqEP, CaptureEP)]
-  | otherwise          = []
+promote :: Square -> Square -> Piece -> Move
+promote sq sq' pp =
+  Move
+  { _origin = sq
+  , _target = sq'
+  , _moveFlag = flag
+  , _captured = Nothing
+  }
   where
-    maskEP = moveMask .&. scopeEP b (c, p) sqEP
-enPassantMoves b sq (c, _) moveMask sqEP = []
+    flag =
+      case pp of
+        N -> PromoteN
+        B -> PromoteB
+        R -> PromoteR
+        Q -> PromoteQ
+        _ -> undefined
+
+capture :: Square -> Square -> (Colour, Piece) -> Move
+capture sq sq' (c, cp) =
+  Move
+  { _origin = sq
+  , _target = sq'
+  , _moveFlag = Capture
+  , _captured = Just (c, cp)
+  }
+
+captureEP :: Square -> Square -> Colour -> Move
+captureEP sq sq' c =
+  Move
+  { _origin = sq
+  , _target = sq'
+  , _moveFlag = CaptureEP
+  , _captured = Just (c, P)
+  }
+
+capturePromote :: Square -> Square -> (Colour, Piece) -> Piece -> Move
+capturePromote sq sq' (c, cp) pp =
+  Move
+  { _origin = sq
+  , _target = sq'
+  , _moveFlag = flag
+  , _captured = Just (c, cp)
+  }
+  where
+    flag =
+      case pp of
+        N -> CapturePromoteN
+        B -> CapturePromoteB
+        R -> CapturePromoteR
+        Q -> CapturePromoteQ
+        _ -> undefined
